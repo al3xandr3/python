dff179b042fb409be218fa706e298bac9269737d
Alexandre Martins <al3xandr3@gmail.com>
2014-10-23 15:03:56 +0300

    up

--- stats.py
+++ stats.py
@@ -7,6 +7,8 @@ def relative_change(new, reference):
 
 # linear regression fit
 def linear_fit (data_ar):
+    import statsmodels.api as sm
+    import pandas as pd
     ndt = data_ar
     X = pd.Series(range(1, len(ndt) + 1))
     y = ndt
@@ -14,9 +16,28 @@ def linear_fit (data_ar):
     fit = sm.OLS(y,sm.add_constant(X)).fit().fittedvalues
     return fit.values
 # linear_fit (['1,2,3,4,5'])
+    fit = sm.OLS(y,sm.add_constant(X)).fit()
+    return {'params': fit.params.values, 'fit_values': fit.fittedvalues.values}
+# linear_fit ([1,2,2,4,4,5])['params']
+# linear_fit ([1,2,2,4,4,5])['fit_values']
 
 # moving average fit
 def mov_avg_fit (data_ar, period):
     X = data_ar
     return pd.rolling_mean(X, period)
 # mov_avg_fit (['1,2,3,4,5'], 2)
+
+def confidence_interval (s):
+    from scipy import stats
+    import scipy as sp
+    import numpy as np
+    import math
+    n, min_max, mean, var, skew, kurt = stats.describe(np.array(s))
+    std=math.sqrt(var)
+    
+    #The location (loc) keyword specifies the mean.
+    #The scale (scale) keyword specifies the standard deviation.
+    # We will assume a normal distribution
+    R = stats.norm.interval(0.975,loc=mean,scale=std/math.sqrt(len(s)))
+    return R
+#confidence_interval([1,2,3,4,4,4,5,5,5,5,4,4,4,6,7,8])
